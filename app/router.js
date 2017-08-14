09a9ffe03e715502c868a77e51089ef268883692
Noah Peden <noahpeden@gmail.com>
2017-08-13 20:22:12 -0600

    Add in each function to dynamically load links.

--- app/router.js
+++ app/router.js
@@ -7,14 +7,20 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  // List of orgs
   this.route('orgs', {});
+
+  // Individual org
   this.route('org', {path: 'org/:id'}, function() {
+    // list of repos
     this.route('repos', {});
-    this.route('repo', {path: ':id'}, function() {
+    // individual repos
+    this.route('repo', {path: ':repoid'}, function() {
       this.route('contributors', {});
       this.route('issues', {});
     });
   });
+  this.route('notfound', {path: '*path'})
 });
 
 export default Router;
